ff0e47d3a5ab6bfd0e9e4f7eb7efe4c6a6d68fc8
yecal <yecal@qq.com>
2019-11-25 12:12:30 +0800

    升级版本，更快更强

--- app/src/main/java/com/yecal/qianding/ui/home/HomeFragment.java
+++ app/src/main/java/com/yecal/qianding/ui/home/HomeFragment.java
@@ -1,24 +1,10 @@
 package com.yecal.qianding.ui.home;
 
-
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.Toast;
-
 import com.yecal.qianding.R;
 import com.yecal.qianding.base.fragment.BaseFragment;
-
-import butterknife.BindView;
-import butterknife.ButterKnife;
-import butterknife.OnClick;
-import butterknife.Unbinder;
 
 /**
  * Created by yecal on 2018/5/30.
